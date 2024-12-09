d10fde5834106b7585cafd9bf7367415ba82d1ef
Carlos Ivan Otero Espinosa <carlosivanoteroespinosa@gmail.com>
2024-12-09 01:25:24 -0500

    Agregar archivo vercel.json y actualizar configuración

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portafolio.Otero',
+  //base: '/Portafolio.Otero', // La base es solo para desplegar en git-hub. No es necesario en vercel
   publicDir: 'public',
   optimizeDeps: {
     exclude: ['lucide-react'],
